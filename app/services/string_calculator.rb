9ade4e4f35721509140ccb72dd7a438395528343
Akhtar <akhtarali@akhtars-MacBook-Pro.local>
2024-12-21 14:38:18 +0530

    Modify add method as class method

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,28 +1,30 @@
 # frozen_string_literal: true
 
 class StringCalculator
-  def add(numbers)
-    return 0 if numbers.empty?
+  class << self
+    def add(numbers)
+      return 0 if numbers.empty?
 
-    delimiter = /,|\n/
-    delimiter, numbers = parse_custom_delimiter(numbers) if numbers.start_with?('//')
-    number_list = numbers.split(delimiter).map(&:to_i)
-    validate_no_negatives(number_list)
-    number_list.sum
-  end
+      delimiter = /,|\n/
+      delimiter, numbers = parse_custom_delimiter(numbers) if numbers.start_with?('//')
+      number_list = numbers.split(delimiter).map(&:to_i)
+      validate_no_negatives(number_list)
+      number_list.sum
+    end
 
-  private
+    private
 
-  def parse_custom_delimiter(numbers)
-    parts = numbers.split("\n", 2)
-    custom_delimiter = Regexp.escape(parts.first[2..-1]).split('').map { |char| '\\' + char }.join('|')
-    [Regexp.new(custom_delimiter), parts.last]
-  end
+    def parse_custom_delimiter(numbers)
+      parts = numbers.split("\n", 2)
+      custom_delimiter = Regexp.escape(parts.first[2..-1]).split('').map { |char| '\\' + char }.join('|')
+      [Regexp.new(custom_delimiter), parts.last]
+    end
 
-  def validate_no_negatives(numbers)
-    negatives = numbers.select(&:negative?)
-    return if negatives.empty?
+    def validate_no_negatives(numbers)
+      negatives = numbers.select(&:negative?)
+      return if negatives.empty?
 
-    raise "negative numbers not allowed: #{negatives.join(', ')}"
+      raise "negative numbers not allowed: #{negatives.join(', ')}"
+    end
   end
 end
