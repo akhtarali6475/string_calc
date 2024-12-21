a46a1f9caad8a80e7a3d12805585a7906750eaff
Akhtar <akhtarali@akhtars-MacBook-Pro.local>
2024-12-21 13:34:06 +0530

    Added functionality to handle exceptions when negative numbers are present in the input string.

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -9,6 +9,19 @@ class StringCalculator
       custom_delimiter, numbers = numbers.split("\n", 2)
       delimiter = Regexp.new(Regexp.escape(custom_delimiter[2..]))
     end
-    numbers.split(delimiter).map(&:to_i).sum
+
+    numbers_list = numbers.split(delimiter).map(&:to_i)
+    validate_no_negatives(numbers_list)
+
+    numbers_list.sum
+  end
+
+  private
+
+  def validate_no_negatives(numbers)
+    negatives = numbers.select(&:negative?)
+    return if negatives.empty?
+
+    raise "negative numbers not allowed: #{negatives.join(', ')}"
   end
 end
